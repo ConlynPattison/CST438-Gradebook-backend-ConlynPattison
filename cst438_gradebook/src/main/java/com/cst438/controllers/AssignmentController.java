e38e7fa782b20cada1398c4c0b2b36733a83d570
ConlynPattison <cpattison@csumb.edu>
2023-09-18 23:19:04 -0700

    added skeleton for AssignmentController endpoints

--- cst438_gradebook/src/main/java/com/cst438/controllers/AssignmentController.java
+++ cst438_gradebook/src/main/java/com/cst438/controllers/AssignmentController.java
@@ -1,54 +1,63 @@
 package com.cst438.controllers;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
 import com.cst438.domain.Assignment;
 import com.cst438.domain.AssignmentDTO;
 import com.cst438.domain.AssignmentRepository;
-import com.cst438.domain.Course;
 import com.cst438.domain.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @RestController
-@CrossOrigin 
+@CrossOrigin
 public class AssignmentController {
-	
-	@Autowired
-	AssignmentRepository assignmentRepository;
-	
-	@Autowired
-	CourseRepository courseRepository;
-	
-	@GetMapping("/assignment")
-	public AssignmentDTO[] getAllAssignmentsForInstructor() {
-		// get all assignments for this instructor
-		String instructorEmail = "dwisneski@csumb.edu";  // user name (should be instructor's email) 
-		List<Assignment> assignments = assignmentRepository.findByEmail(instructorEmail);
-		AssignmentDTO[] result = new AssignmentDTO[assignments.size()];
-		for (int i=0; i<assignments.size(); i++) {
-			Assignment as = assignments.get(i);
-			AssignmentDTO dto = new AssignmentDTO(
-					as.getId(), 
-					as.getName(), 
-					as.getDueDate().toString(), 
-					as.getCourse().getTitle(), 
-					as.getCourse().getCourse_id());
-			result[i]=dto;
-		}
-		return result;
-	}
-	
-	// TODO create CRUD methods for Assignment
+
+    @Autowired
+    AssignmentRepository assignmentRepository;
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @GetMapping("/assignment")
+    public AssignmentDTO[] getAllAssignmentsForInstructor() {
+        // get all assignments for this instructor
+        String instructorEmail = "dwisneski@csumb.edu";  // user name (should be instructor's email)
+        List<Assignment> assignments = assignmentRepository.findByEmail(instructorEmail);
+        AssignmentDTO[] result = new AssignmentDTO[assignments.size()];
+        for (int i = 0; i < assignments.size(); i++) {
+            Assignment as = assignments.get(i);
+            AssignmentDTO dto = new AssignmentDTO(
+                    as.getId(),
+                    as.getName(),
+                    as.getDueDate().toString(),
+                    as.getCourse().getTitle(),
+                    as.getCourse().getCourse_id());
+            result[i] = dto;
+        }
+        return result;
+    }
+
+    @PostMapping("/assignment")
+    @Transactional
+    public ResponseEntity<Void> createAssignment(@RequestBody Assignment assignment) {
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping("/assignment/{id}")
+    @Transactional
+    public ResponseEntity<Void> updateAssignment(@RequestBody Assignment assignment, @PathVariable("id") Integer assignmentId) {
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @DeleteMapping("/assignment/{id}")
+    @Transactional
+    public ResponseEntity<Void> deleteAssignment(@PathVariable("id") Integer assignmentId) {
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    // TODO create CRUD methods for Assignment
 }
