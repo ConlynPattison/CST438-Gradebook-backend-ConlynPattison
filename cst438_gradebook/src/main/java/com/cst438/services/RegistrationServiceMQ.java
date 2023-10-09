79713bc97f0b1d8c8556b3c0b18b71013dd36904
ConlynPattison <cpattison@csumb.edu>
2023-10-08 22:59:38 -0700

    Added RabbitMQ receive/send messaging w/Registration

--- cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceMQ.java
+++ cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceMQ.java
@@ -1,80 +1,94 @@
 package com.cst438.services;
 
 
+import com.cst438.domain.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import com.cst438.domain.Course;
-import com.cst438.domain.FinalGradeDTO;
-import com.cst438.domain.CourseRepository;
-import com.cst438.domain.Enrollment;
-import com.cst438.domain.EnrollmentDTO;
-import com.cst438.domain.EnrollmentRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @ConditionalOnProperty(prefix = "registration", name = "service", havingValue = "mq")
 public class RegistrationServiceMQ implements RegistrationService {
 
-	@Autowired
-	EnrollmentRepository enrollmentRepository;
+    @Autowired
+    EnrollmentRepository enrollmentRepository;
 
-	@Autowired
-	CourseRepository courseRepository;
+    @Autowired
+    CourseRepository courseRepository;
 
-	@Autowired
-	private RabbitTemplate rabbitTemplate;
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
-	public RegistrationServiceMQ() {
-		System.out.println("MQ registration service ");
-	}
+    public RegistrationServiceMQ() {
+        System.out.println("MQ registration service ");
+    }
 
 
-	Queue registrationQueue = new Queue("registration-queue", true);
+    Queue registrationQueue = new Queue("registration-queue", true);
 
-	/*
-	 * Receive message for student added to course
-	 */
-	@RabbitListener(queues = "gradebook-queue")
-	@Transactional
-	public void receive(String message) {
-		
-		System.out.println("Gradebook has received: "+message);
+    /*
+     * Receive message for student added to course
+     */
+    @RabbitListener(queues = "gradebook-queue")
+    @Transactional
+    public void receive(String message) {
 
-		//TODO  deserialize message to EnrollmentDTO and update database
-	}
+        System.out.println("Gradebook has received: " + message);
 
-	/*
-	 * Send final grades to Registration Service 
-	 */
-	@Override
-	public void sendFinalGrades(int course_id, FinalGradeDTO[] grades) {
-		 
-		System.out.println("Start sendFinalGrades "+course_id);
+        EnrollmentDTO enrollmentDTO = fromJsonString(message, EnrollmentDTO.class);
 
-		//TODO convert grades to JSON string and send to registration service
-		
-	}
-	
-	private static String asJsonString(final Object obj) {
-		try {
-			return new ObjectMapper().writeValueAsString(obj);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+        // Get course or throw
+        Course course = courseRepository.findById(enrollmentDTO.courseId()).orElseThrow(() -> {
+                    throw new ResponseStatusException(
+                            HttpStatus.NOT_FOUND,
+                            "Course id " + enrollmentDTO.courseId() + " not found");
+                }
+        );
 
-	private static <T> T  fromJsonString(String str, Class<T> valueType ) {
-		try {
-			return new ObjectMapper().readValue(str, valueType);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+        Enrollment enrollment = new Enrollment();
+        enrollment.setCourse(course);
+        enrollment.setStudentName(enrollmentDTO.studentName());
+        enrollment.setStudentEmail(enrollmentDTO.studentEmail());
+        // TODO: should this id be decided by the other server's request?
+        enrollment.setId(enrollmentDTO.id());
+
+        enrollmentRepository.save(enrollment);
+    }
+
+    /*
+     * Send final grades to Registration Service
+     */
+    @Override
+    public void sendFinalGrades(int course_id, FinalGradeDTO[] grades) {
+
+        System.out.println("Start sendFinalGrades " + course_id);
+
+        //TODO convert grades to JSON string and send to registration service
+        String finalGradeData = asJsonString(grades);
+        rabbitTemplate.convertAndSend(registrationQueue.getName(), finalGradeData);
+    }
+
+    private static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static <T> T fromJsonString(String str, Class<T> valueType) {
+        try {
+            return new ObjectMapper().readValue(str, valueType);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
